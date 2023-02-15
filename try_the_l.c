5cf3ca19ced85188f3bfd9cea5fb2877f5478cd7
Mohammed EZ-ZOUAK <120412811+MedEZZOUAK@users.noreply.github.com>
2023-02-15 14:33:27 +0100

    v2 of the first code with some test

--- try_the_l.c
+++ try_the_l.c
@@ -9,7 +9,7 @@
 struct Transition {
   int from;
   int to;
-  char symbol[100];
+  char symbol[MAX_SYMBOLS];
 };
 
 
@@ -52,6 +52,7 @@ void write_automate_to_file(char *filename, struct Automate automate) {
   // Close the file
   fclose(file);
 }
+
 // Function to read an Automate structure from a file
 void read_automate_from_file(char *filename, struct Automate *automate) {
   // Open the file for reading
@@ -121,8 +122,84 @@ void create_dot_file(char *filename, struct Automate automate) {
   fprintf(file, "}\n");
   fclose(file);
 }
-int main(){
 
+// Download and install Graphviz: Go to the Graphviz website (https://www.graphviz.org/), and follow the instructions to download and install the software.
+
+// Open Graphviz: Start Graphviz, and you'll see the "Graphviz DOT Editor" window.
+
+// Load the .dot file: Click on "File" and then "Open". Locate your .dot file and select it. The file will be loaded into the editor.
+
+// Generate the graph: To generate the graph, click on "File" and then "Generate Graph". A window will pop up, asking you to choose the format of the output file. You can choose any format you like, but PNG is a popular choice. After choosing the format, select a location to save the generated graph and click "Save".
+
+// View the generated graph: The generated graph will be saved as an image file. You can open the image file with any image viewer, such as Windows Photo Viewer or Microsoft Paint.
+
+int main(){
+  
+   struct Automate a1 = {};
+
+
+  // Declare the automaton
+  struct Automate A1 = {
+    .nb_states = 3,
+    .initial_state = 0,
+    .nb_final_states = 1,
+    .final_states = {2},
+    .transition_table = {
+      // a
+      {1, 0, 0},
+      // b
+      {0, 2, 0},
+      // c
+      {0, 0, 2},
+    },
+  };
+
+  // Test the automaton with some input strings
+  char input1[] = "abcab";
+  int current_state = A1.initial_state;
+
+  for (int i = 0; i < strlen(input1); i++) {
+    char symbol = input1[i];
+    int symbol_index = symbol - 'a'; // Assumes symbols are characters 'a', 'b', 'c', ...
+
+    current_state = A1.transition_table[current_state][symbol_index];
+  }
+
+  // Check if the final state is an accepting state
+  int is_accepting = 0;
+  for (int i = 0; i < A1.nb_final_states; i++) {
+    if (current_state == A1.final_states[i]) {
+      is_accepting = 1;
+      break;
+    }
+  }
+
+  printf("Input string: %s\n", input1);
+  printf("Final state: %d\n", current_state);
+  printf("Is accepting: %s\n", is_accepting ? "yes" : "no");
+
+  char input2[] = "aabbc";
+  current_state = A1.initial_state;
+
+  for (int i = 0; i < strlen(input2); i++) {
+    char symbol = input2[i];
+    int symbol_index = symbol - 'a'; // Assumes symbols are characters 'a', 'b', 'c', ...
+
+    current_state = A1.transition_table[current_state][symbol_index];
+  }
+
+  // Check if the final state is an accepting state
+  is_accepting = 0;
+  for (int i = 0; i < A1.nb_final_states; i++) {
+    if (current_state == A1.final_states[i]) {
+      is_accepting = 1;
+      break;
+    }
+  }
+  
+  printf("Input string: %s\n", input2);
+  printf("Final state: %d\n", current_state);
+  printf("Is accepting: %s\n", is_accepting ? "yes" : "no");
     return 0 ;
-    
+
 }
